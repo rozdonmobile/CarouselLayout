f8109e8ff320788f134944e4e025bdbb28ba05c8
Max Rozdobudko <max.rozdobudko@gmail.com>
2017-05-24 09:11:43 +0300

    initial commit

---
+++ CarouselLayout/CarouselLayout.swift
@@ -0,0 +1,98 @@
+//
+//  CarouselLayout.swift
+//  CarouselLayout
+//
+//  Created by Max Rozdobudko on 5/23/17.
+//  Copyright © 2017 Noisy Miner. All rights reserved.
+//
+
+import UIKit
+
+class CarouselLayout: UICollectionViewLayout {
+    
+    public enum ScrollDirection: String {
+        case horizontal
+        case vertical
+    }
+    
+    fileprivate var _attributes: [IndexPath : UICollectionViewLayoutAttributes] = [:];
+    fileprivate var _contentSize: CGSize?
+    
+    open var scrollDirection: ScrollDirection = .horizontal
+    open var contentInset: UIEdgeInsets = UIEdgeInsetsMake(20, 20, 20, 20)
+    
+    override var collectionViewContentSize: CGSize {
+        return _contentSize ?? super.collectionViewContentSize
+    }
+
+    override func prepare() {
+        super.prepare();
+        
+        guard let collectionView = self.collectionView else {
+            return
+        }
+        
+        switch scrollDirection {
+        case .horizontal:
+            var xPos: CGFloat = 0;
+            var yPos: CGFloat = 0;
+            
+            for section in 0..<collectionView.numberOfSections {
+                yPos += contentInset.top
+                for item in 0..<collectionView.numberOfItems(inSection: section) {
+                    let indexPath = IndexPath(item: item, section: section)
+                    
+                    xPos += contentInset.left;
+                    
+                    let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+                    attributes.frame = CGRect(origin: CGPoint(x: xPos, y: yPos), size: cellSize(for: collectionView))
+                    
+                    xPos = xPos + attributes.frame.size.width + contentInset.right //attributes.frame.maxX;
+                    
+                    _attributes[indexPath] = attributes;
+                }
+                
+                yPos = yPos + cellSize(for: collectionView).height + contentInset.bottom //attributes.frame.maxY;
+            }
+            
+            _contentSize = CGSize(width: xPos, height: yPos);
+        default:
+            break;
+        }
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var result: [UICollectionViewLayoutAttributes] = []
+        for attributes in _attributes {
+            if attributes.value.frame.intersects(rect) {
+                result.append(attributes.value)
+            }
+        }
+        return result
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        return _attributes[indexPath];
+    }
+    
+    func cellSize(for collectionView: UICollectionView?) -> CGSize {
+        if let collectionView = collectionView {
+            return CGSize(width: collectionView.frame.width - contentInset.left - contentInset.right,
+                          height: collectionView.frame.height - contentInset.top - contentInset.bottom)
+        } else {
+            return .zero;
+        }
+    }
+    
+    func totalItems(in collectionView: UICollectionView?) -> Int {
+        var result = 0;
+        if let collectionView = collectionView {
+            for section in 0..<collectionView.numberOfSections {
+                for _ in 0..<collectionView.numberOfItems(inSection: section) {
+                    result += 1
+                }
+            }
+        }
+        return result
+    }
+}
---
+++ CarouselLayout/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CarouselLayout
+//
+//  Created by Max Rozdobudko on 5/23/17.
+//  Copyright © 2017 Noisy Miner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
